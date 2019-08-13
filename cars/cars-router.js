ab4c2fae074f20477109c45e61aab8e8d0e06fc8
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-13 14:45:30 -0600

    Added a delete and a put method

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -15,6 +15,7 @@ const db = knex(knexConfig.development);
 
 const router = express.Router();
 
+// Get all cars from the db
 router.get("/", (req, res) => {
   db("cars")
     .then(cars => {
@@ -27,6 +28,7 @@ router.get("/", (req, res) => {
     });
 });
 
+// Add a new car to the db
 router.post("/", (req, res) => {
   const newCar = req.body;
   db("cars")
@@ -38,6 +40,35 @@ router.post("/", (req, res) => {
       res
         .status(500)
         .json({ message: "Could not retrieve list of cars from the db" });
+    });
+});
+
+//Delete a car from the db using it's ID
+router.delete("/:id", (req, res) => {
+  const carId = req.params.id;
+  db("cars")
+    .where("id", "=", carId)
+    .del()
+    .then(car => {
+      res.status(200).json(car);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not delete car from the db" });
+    });
+});
+
+//Update a car in the db
+router.put("/:id", (req, res) => {
+  const carId = req.params.id;
+  const changes = req.body;
+  db("cars")
+    .where("id", "=", carId)
+    .update(changes)
+    .then(car => {
+      res.status(200).json(car);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not update that car in the db" });
     });
 });
 
